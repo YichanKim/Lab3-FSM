2e1e6c2e37d33528b71b295e1489c4e6fab24589
Yichan Kim <yichankim@gmail.com>
2025-11-01 21:35:14 +0000

    lab 3

--- task3/clktick.sv
+++ task3/clktick.sv
@@ -1,3 +1,5 @@
+//optimal N value is 24
+
 module clktick #(
     parameter WIDTH = 16
 )(
@@ -12,18 +14,18 @@ module clktick #(
 logic [WIDTH-1:0] count;
 
 always_ff @ (posedge clk)
-    if (rst) begin
-        tick <= 1'b0;
-        count <= N;  
+    if (rst) begin                   // common practice to have being/end for all if
+        tick <= 1'b0;                // output a 0
+        count <= N;                  // set count back to N
         end
     else if (en) begin
         if (count == 0) begin
-            tick <= 1'b1;
-            count <= N;
+            tick <= 1'b1;            // set output to 1
+            count <= N;              // reset count
             end
         else begin
-            tick <= 1'b0;
-            count <= count - 1'b1;
+            tick <= 1'b0;            // set output back to 0
+            count <= count - 1'b1;   // decrement count by 1
             end
         end
 endmodule
